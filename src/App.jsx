5af04328eae668d8266d54a46b7802a6b5028880
Avinash Thakur <avinash.thakur2209@gmail.com>
2025-03-22 01:05:09 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserModal from './components/UserModal'
+
+function App() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <h1>User Details Modal</h1>
+      <button style={{background: 'lightblue'}} onClick={() => setOpen(true)}>Open Form</button>
+      <UserModal open={open} setOpen={setOpen}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UserModal.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./UserModal.css";
+
+function UserModal({open, setOpen}) {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        dob: ''
+    });
+    function handleSubmit(e){
+        e.preventDefault();
+        if(formData.phone.length < 10){
+            alert('Invalid phone number. Please enter a 10-digit phone number.');
+        }
+        else if(Date.now() < new Date(formData.dob).getTime()){
+            alert('Invalid date of birth. Date of Birth cannot be in the future.');
+        }
+        else{
+            setFormData({
+                name: '',
+                email: '',
+                phone: '',
+                dob: ''
+            });
+        }
+    }
+  return (
+    <div className="modal" style={{display : open ? 'block' : 'none'}} onClick={() => setOpen(false)}>
+      <div className="modal-content">
+        <h3>Fill Details</h3>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input name="username" id='username' type="text" value={formData.name} required onChange={(e) => setFormData(prev => ({...prev, name: e.target.value}))}></input>
+            <label htmlFor="email">Email Address:</label>
+            <input name="email" id='email' type='email' required value={formData.email} onChange={(e) => setFormData(prev => ({...prev, email: e.target.value}))}></input>
+            <label htmlFor="phone">Phone Number:</label>
+            <input name="phone" id='phone' type='number' required value={formData.phone} onChange={(e) => setFormData(prev => ({...prev, phone: e.target.value}))}></input>
+            <label htmlFor="dob">Date of Birth:</label>
+            <input name="dob" id='dob' type='date' required value={formData.dob} onChange={(e) => setFormData(prev => ({...prev, dob: e.target.value}))}></input>
+            <button style={{width: '30%'}}type="submit" className="submit-button">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default UserModal;
